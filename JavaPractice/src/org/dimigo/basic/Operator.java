f88d62405092af105868c9d584ecfe883cea43f3
Minseo-Laptop <Minseo-Laptop@LAPTOP-CHR0380>
2017-03-14 12:59:40 +0900

    실습과제 2

---
+++ JavaPractice/src/org/dimigo/basic/Operator.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package org.dimigo.basic;
+
+/**
+ *<pre>
+ * org.dimigo.basic
+ *   └ Operator
+ *
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 3. 14.
+ * </pre>
+ *  
+ * @author : Minseo-Laptop
+ * @version : 1.0
+ */
+public class Operator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int month = 1700000;
+		int supply = 3;
+		int shop = 1500;
+		System.out.println("<<디미베네 연간 인건비>>");
+		System.out.println("월 평균 급여 : "+ String.format("%,d",month)+'원');
+		System.out.println("점포 내 직원 수 : "+ supply + '명');
+		System.out.println("점포 수 : " + String.format("%,d", shop)+'개');
+		System.out.println("\n\n"+"연간 인건비 : " + String.format("%,d", (long)month*12*supply*shop)+'원');
+		
+		
+
+	}
+
+}
